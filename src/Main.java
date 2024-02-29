af35f4d9639f7a1c158a0962772d7184e97ca48a
KonstantinMikheev <mikhey17@gmail.com>
2024-02-29 19:40:05 +0300

    HW conditions #1 with ternary operator

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int givenAge = 18
+        int givenAge = 15;
         boolean ageIsEqualsOrMore18 = givenAge >= 18;
         System.out.println("Если возраст человека равен " + givenAge);
         if (ageIsEqualsOrMore18) {
@@ -8,5 +8,8 @@ public class Main {
         } else {
             System.out.println("Ты не достиг совершеннолетия, нужно немного подождать.");
         }
+    int myAge = 32;
+    String isIAmAdult = (myAge >= 18) ? "Ты совершеннолетний" : "Ты не совершеннолетний";
+    System.out.println(isIAmAdult);
     }
 }
